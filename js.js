c983ad0d40fd389564c77e5198fae373328fd3b9
szt2makpet <szt2makpet@vasvari.org>
2023-10-10 12:14:48 +0200

    kész.

--- js.js
+++ js.js
@@ -1,33 +1,33 @@
 const hetnapjai = ["hétfő", "kedd", "szerda", "csütörtök", "péntek", "szombat", "vasárnap"];
 
-const randomIndex0 = 2;
+const ranIndex = 2;
 
-const ujTomb = [hetnapjai[randomIndex0]];
+const tomb = [hetnapjai[ranIndex]];
 
-const randomIndex1 = 5;
+const seged = 5;
 
-ujTomb.push(hetnapjai[randomIndex1]);
-hetnapjai.splice(randomIndex1, 1);
+tomb.push(hetnapjai[seged]);
+hetnapjai.splice(seged, 1);
 
-const randomIndex2 = 1;
+const seged2 = 1;
 
-ujTomb.push(hetnapjai[randomIndex2]);
-hetnapjai.splice(randomIndex2, 1);
+tomb.push(hetnapjai[seged2]);
+hetnapjai.splice(seged2, 1);
 
-const randomIndex3 = 0;
+const seged3 = 0;
 
-ujTomb.push(hetnapjai[randomIndex3]);
-hetnapjai.splice(randomIndex3, 1);
+tomb.push(hetnapjai[seged3]);
+hetnapjai.splice(seged3, 1);
 
-const randomIndex4 = 2;
+const seged4 = 2;
 
-ujTomb.push(hetnapjai[randomIndex4]);
-hetnapjai.splice(randomIndex4, 1);
+tomb.push(hetnapjai[seged4]);
+hetnapjai.splice(seged4, 1);
 
-const randomIndex5 = 0;
+const seged5 = 0;
 
-ujTomb.push(hetnapjai[randomIndex5]);
-hetnapjai.splice(randomIndex5, 1);
+tomb.push(hetnapjai[seged5]);
+hetnapjai.splice(seged5, 1);
 
-console.log("Új tömb:", ujTomb);
+console.log("Új tömb:", tomb);
 console.log("Eredeti tömb maradék elemei:", hetnapjai);
